37ec8eb9199eb99de6662f0e3b1b22a44c969fdc
Pablo Zubieta <pabloferz@yahoo.com.mx>
2020-01-08 13:13:32 -0600

    Add LiquidsDynamics to build script

--- deps/build.jl
+++ deps/build.jl
@@ -2,3 +2,4 @@ using Pkg
 
 Pkg.add(PackageSpec(url="https://github.com/LANIMFE/ApproximationGrids.jl.git")
 Pkg.add(PackageSpec(url="https://github.com/LANIMFE/LiquidsStructure.jl.git")
+Pkg.add(PackageSpec(url="https://github.com/LANIMFE/LiquidsDynamics.jl.git")
